ee707090cf3d824726a12fa671f417b23777ec6f
Itz-tisi <b83795814@gmail.com>
2025-01-22 12:44:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ BOT_TOKEN = "7461925686:AAHiQp1RS7YAVFVVHoWEyKgaE5wGYgO0QJo"
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Hello! I'm your bot.")
 
-app = ApplicationBuilder().token(7461925686:AAHiQp1RS7YAVFVVHoWEyKgaE5wGYgO0QJo).build()
+app = ApplicationBuilder().token("7461925686:AAHiQp1RS7YAVFVVHoWEyKgaE5wGYgO0QJo").build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
